8f44c8ff2d9df0bdd42db36bfe054e9a1ead1c30
vignesh-ravikumar <vignesh.ravikumar@rently.com>
2024-06-10 14:46:47 +0530

    clang-format for new changes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
--- main.h
+++ main.h
@@ -12,18 +12,18 @@ typedef struct {
 
 /**
  * @brief Creates a new node using the input data
- * 
+ *
  * @param data Character data to be stored in the node
  * @return Pointer to the created node
-*/
+ */
 Node *createNode(char data);
 
 /**
  * @brief Inserts a new node at the tail of linked list
- * 
+ *
  * @param list Pointer to the linked list
  * @param data Character data to be inserted in the new node
-*/
+ */
 void insertAtTail(LinkedList *list, char data);
 
 /**
@@ -35,18 +35,18 @@ void initializeLinkedList(LinkedList *list);
 
 /**
  * @brief Saves the string in the character linked List
- * 
+ *
  * @param in_str Pointer to the input string
  * @param list Pointer to the linked list
-*/
+ */
 void saveStrInLinkedList(char *in_str, LinkedList *list);
 
 /**
  * @brief Prints the word in reverse
- * 
+ *
  * @param in_str Pointer to start node of word
  * @param list Pointer to end node of word
-*/
+ */
 void printWordReverse(Node *word_strt, Node *word_end);
 
 /**
